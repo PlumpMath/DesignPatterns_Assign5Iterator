96cdb65a99a1a78c4d28d4e9138b108cc330ae6a
jfelts1 <jf.felts1@gmail.com>
2015-05-06 13:24:56 -0700

    inital Commit

---
+++ DesignPatterns_Assign5Iterator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns_Assign5Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
